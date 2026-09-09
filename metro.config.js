32c31d4c982a8c4fd9ff325abd0f5877b5a2fbb8
Eduardo Bielecky <eduardombielecky@gmail.com>
2026-09-09 14:23:39 -0300

    feat: configure metro.config.js to support expo-sqlite web assembly and cross-origin headers

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// Permite ao Metro empacotar o arquivo WebAssembly do expo-sqlite na Web
+config.resolver.assetExts.push('wasm');
+
+// Configura os cabeçalhos HTTP necessários para o WebAssembly e SharedArrayBuffer do SQLite na Web
+config.server = config.server || {};
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    return middleware(req, res, next);
+  };
+};
+
+module.exports = config;
